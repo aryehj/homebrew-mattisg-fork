2460985e59ee5f610a02d1f720aed38df4a736ec
Matti Schneider <hi@mattischneider.fr>
2015-07-14 10:52:10 +0200

    Add description to adblock formula

--- adblock.rb
+++ adblock.rb
@@ -3,6 +3,8 @@ class Adblock < Formula
   head "https://github.com/mattisg/adblock.git"
   url "https://github.com/mattisg/adblock.git", :tag => "v1.0.1"
 
+  desc "Block ads and trackers system-wide, before a request is even issued."
+
   depends_on "bash"
 
   def install
